48e4da549ced7cd2f02e13bf3ba8caf7436468ff
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2025-04-22 12:00:38 +0300

    Update 1-multiply.swift

--- Swift/ProjectEuler/1-multiply.swift
+++ Swift/ProjectEuler/1-multiply.swift
@@ -1,13 +1,16 @@
 func multiply(_ until : Int) -> Int{
     var result: Int = 0
+    var array: [Int] = []
     for n in 1...until - 1 {
         if n % 3 == 0 || n % 5 == 0 {
             result += n
+            array.append(n)
         }
     }
+    print(array)
     return result
 }
 
-var res = multiply(1000)
+var res = multiply(10)
 print(res)
 
