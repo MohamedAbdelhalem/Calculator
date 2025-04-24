f8319297878613ac57fc970fc84a54f22161e222
Mohamed Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2025-04-24 17:33:04 +0300

    Update 5-SmallestMultiple.swift

--- Swift/ProjectEuler/5-SmallestMultiple.swift
+++ Swift/ProjectEuler/5-SmallestMultiple.swift
@@ -1,18 +1,21 @@
+var until : Int = 20
 var num : Int = 0
 var temp : Int = 0
 var list : [Int] = []
 var allLists : [Int] = []
-for x in 1...9999 {
-    for i in 1...10 {
+for x in 1...9999999999 {
+    for i in 1...until {
         if (Float(x) / Float(i)).truncatingRemainder(dividingBy: 1) == 0.0  && temp == 0 {
             temp += 1
             num += 1
         }
         temp = 0
     }
-    if num > 10{
+    if num > until {
         list += [x]
+        break
     }
     num = 1
 }
 print(list[0])
+print("Done")
